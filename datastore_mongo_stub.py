67184cc70dc55b839abc6c1c59194268a22d8f6b
Mike Dirolf <mike@10gen.com>
2009-03-09 17:13:59 -0400

    add 10gen to copyright in addition to google

--- datastore_mongo_stub.py
+++ datastore_mongo_stub.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright 2007 Google Inc.
+# Copyright 2007 Google Inc., 2008-2009 10gen Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
